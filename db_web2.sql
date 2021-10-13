592ab6cdd49561042ecc69801127c87160dde4f3
Franco Neuville <franneuville@gmail.com>
2021-10-13 18:16:53 -0300

    actualizacion db

--- db_web2.sql
+++ db_web2.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-10-2021 a las 23:07:49
--- Versión del servidor: 10.4.21-MariaDB
--- Versión de PHP: 8.0.11
+-- Tiempo de generación: 13-10-2021 a las 18:10:57
+-- Versión del servidor: 10.4.19-MariaDB
+-- Versión de PHP: 8.0.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -37,12 +37,12 @@ CREATE TABLE `marcas` (
 --
 
 INSERT INTO `marcas` (`id_marca`, `nombre`) VALUES
-(1, 'Asus'),
-(2, 'MSI'),
-(3, 'Samsung'),
-(4, 'HP'),
-(5, 'Razer'),
-(6, 'Dell');
+(5, 'Samsung'),
+(12, 'Asus'),
+(17, 'HP'),
+(18, 'Razer'),
+(19, 'Dell'),
+(20, 'MSI');
 
 -- --------------------------------------------------------
 
@@ -64,13 +64,15 @@ CREATE TABLE `notebooks` (
 --
 
 INSERT INTO `notebooks` (`id_notebook`, `procesador`, `memoria_ram`, `placa_video`, `disco`, `fk_marca`) VALUES
-(13, 'Intel i5 7300hq', '8gb', 'GTX 1050', '500gb', 3),
 (15, 'Intel i5 7300hq', '32gb', 'GTX 1060', '500gb', 5),
-(17, 'Intel i5 7200u', '8gb', NULL, '500gb', 3),
-(19, 'Intel i5 7200u', '8gb', NULL, '500gb', 3),
 (20, 'Intel i5 7300hq', '16gb', 'GTX 1050', '500gb', 5),
-(21, 'Intel i5 7300hq', '16gb', 'GTX 1060', '500gb', 1),
-(22, 'Intel i7 7700hq', '16gb', 'GTX 1070', '1000gb', 4);
+(34, 'Intel i5 7800k', '16 GB', 'RXT 1080', '1000GB', 12),
+(35, 'Ryzen 3 3600', '8 GB', 'RTX 2050 ti', '250 GB', 12),
+(36, 'Ryzen 5 2600 ', '8 GB', 'GTX 1060', '1000GB', 17),
+(37, 'Intel i7 5700', '16 GB', 'RTX 1600 ', '1000GB', 17),
+(38, 'Ryzen 7 2600 ', '16 GB', 'RXT 1050 ti', '1000GB', 18),
+(39, 'Intel i9 9900', '16 GB', 'RXT 1080 ti', '250 GB', 18),
+(40, 'Intel i7 8800', '16 GB', 'GTX 2060 ti', '250 GB', 19);
 
 -- --------------------------------------------------------
 
@@ -93,9 +95,9 @@ CREATE TABLE `usuarios` (
 INSERT INTO `usuarios` (`id_usuario`, `usuario`, `contraseña`, `email`, `administrador`) VALUES
 (10, 'ahorasi', '$2y$10$VCyGjl7HigRrOP/bQnpR6uElUjYZtL3q49Pau0OwNyUnM9PMWT2Oi', 'ahorasi@ahora.com', 1),
 (11, 'fran1', '$2y$10$9HIUlmvxZGD4YMo94L0xveujHZc6pI4QKtuhnr8R2YZkSn0chrYw6', 'fran1@fran.com', NULL),
-(15, 'poyy', '$2y$10$N66hOLQe0H7xDy6Q8HVFXOlbR7cEODIbJsHNhOfTQr5.pTLW.xx0W', 'poyy@poyy.com', 1),
+(15, 'poyy', '$2y$10$N66hOLQe0H7xDy6Q8HVFXOlbR7cEODIbJsHNhOfTQr5.pTLW.xx0W', 'poyy@poyy.com', NULL),
 (16, 'gato11', '$2y$10$ksnL8KeU.TkH9CxwrBzrqeP2IkVdC6J34U3gmWIaRcHWEDJZ0FJ2K', 'gato11@gato.com', NULL),
-(17, 'valen', '$2y$10$/OsQ8Umbuqf2S6HFIlgTwOWQv3tbAt5mYjGoqV5mbsMKU/VgGSa0q', 'valen@valen.com', NULL);
+(18, 'hola', '$2y$10$h1YQ9vZdwNN9zCDN3qZZbu1F/tTORlN6BS0sjLJOE/9IqQEgnRXuu', 'hola@hola.com', NULL);
 
 --
 -- Índices para tablas volcadas
@@ -129,19 +131,19 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `marcas`
 --
 ALTER TABLE `marcas`
-  MODIFY `id_marca` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_marca` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT de la tabla `notebooks`
 --
 ALTER TABLE `notebooks`
-  MODIFY `id_notebook` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id_notebook` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- Restricciones para tablas volcadas
